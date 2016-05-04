e003acf5c72f2ceddce4b0149a5be3414adeccf3
Carlos Queiroz <caxqueiroz@gmail.com>
2016-05-04 15:58:01 +0800

    first commit.

---
+++ src/main/java/com/example/DeployStreamsApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.xd.rest.client.impl.SpringXDTemplate;
+import org.springframework.xd.rest.domain.StreamDefinitionResource;
+
+import java.net.URI;
+
+@SpringBootApplication
+public class DeployStreamsApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployStreamsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		SpringXDTemplate xdt = new SpringXDTemplate(new URI("http://localhost:9393"));
+
+        //java.lang.String name,java.lang.String definition,boolean deploy
+        StreamDefinitionResource streamDefinitionResource = xdt.streamOperations().createStream("test-stream","time | log",true);
+
+
+    }
+}
